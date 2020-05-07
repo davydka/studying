e85752ec37e49fe6977695be9133793d8ad2bbb5
davydka <davydka@gmail.com>
2020-05-07 11:57:49 -0700

    studying

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// make a charMap for stringA and stringB
+// make charMap helper function
+
+const buildCharMap = (str) => {
+  const charMap = {};
+  for (let c of cleanString(str)) {
+    charMap[c] = charMap[c] + 1 || 1;
+  }
+  return charMap;
+}
+
+const cleanString = (str, doSort = false) => {
+  const cleaned = str.replace(/[^\w]/g, '').toLowerCase();
+  if (!doSort) {
+    return cleaned;
+  }
+   return cleaned.split('').sort().join('');
+}
+
+const answer1 = (stringA, stringB) => {
+  const charMapA = buildCharMap(stringA);
+  const charMapB = buildCharMap(stringB);
+
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false;
+  }
+
+  return Object.keys(charMapA).reduce((a, c,) => charMapA[c] === charMapB[c], true);
+}
+
+const answer2 = (stringA, stringB) => {
+  return cleanString(stringA, true) === cleanString(stringB, true);
+}
+
+function anagrams(stringA, stringB) {
+  return answer2(stringA, stringB);
+}
+
+// const test = anagrams('RAIL! SAFETY!', 'fairy tales');
+// debugger;
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,48 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+const answer1 = (array, size) => {
+  let counter = 0;
+  const target = array.reduce((a, c, i) => {
+    if (i > 0 && i % size === 0) {
+      counter++;
+      a[counter] = [];
+    }
+    a[counter].push(c);
+    return a;
+  }, [[]]);
+  return target;
+}
+
+const answer2 = (array, size) => {
+  const target = [];
+  for (let item of array) {
+    const last = target[target.length - 1];
+    if (!last || last.length === size) {
+      target.push([item]);
+    } else {
+      last.push(item);
+    }
+  }
+  return target;
+}
+
+const answer3 = (array, size) => {
+  const target = [];
+  let count = 0;
+  while (count < array.length) {
+    target.push(array.slice(count, count + size));
+    count += size;
+  }
+  return array;
+}
+
+
+function chunk(array, size) {
+  return answer2(array, size);
+}
+
+const test = chunk([1,2,3,4,5,6], 4);
+debugger;
 
 module.exports = chunk;
