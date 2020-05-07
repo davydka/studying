fd0d4447db90818f1d6bfd089f3dec22a23d2b56
davydka <davydka@gmail.com>
2020-05-07 14:41:27 -0700

    studying

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,30 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+const answer1 = (str) => {
+  return str.split(' ').map(w => {
+    const target = w.split('');
+    target[0] = target[0].toUpperCase();
+    return target.join('');
+  }).join(' ');
+}
+
+const answer2 = (str) => {
+  return str.split(' ').map(w => w[0].toUpperCase() + w.slice(1)).join(' ');
+}
+
+const answer3 = (str) => {
+  return str.split('').reduce((a, c, i) => {
+    a.push(a[a.length - 1] === ' ' || i === 0 ? c.toUpperCase() : c);
+    return a;
+  }, ['']).join('');
+}
+
+function capitalize(str) {
+  return answer3(str);
+}
+
+// const test = capitalize('hi there, how is it going?');
+// debugger;
 
 module.exports = capitalize;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,40 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+const answer1 = (n) => {
+  for (let i = 0; i < n; i++) {
+    let target = '';
+    for (let j = 0; j < n; j++) {
+      target += j <= i ? '#' : ' ';
+    }
+    console.log(target);
+  }
+}
+
+// recursive solution
+const answer2 = (n) => {
+  print(n, 0);
+  return n;
+}
+
+const print = (n, row, stair = '') => {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return print(n, row+1);
+  }
+
+  stair += stair.length <= row ? '#' : ' ';
+  print(n, row, stair);
+}
+
+function steps(n) {
+  return answer2(n);
+}
+
+steps(4)
 
 module.exports = steps;
