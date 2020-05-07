0f576b7644d9ac5a15a166cfb3b418b78e5eaca7
davydka <davydka@gmail.com>
2020-05-07 16:51:55 -0700

    studying

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,57 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######'
+//   pyramid(5)
+//       '    #    '
+//       '   ###   '
+//       '  #####  '
+//       ' ####### '
+//       '#########'
 
-function pyramid(n) {}
+// iterative solution
+const answer1 = (n) => {
+  const cols = (n * 2) - 1;
+  const colMid = Math.floor(cols / 2);
+
+  for (let row = 0; row < n; row++){
+    let level = '';
+    for (let col = 0; col < cols; col++) {
+      level += (col >= colMid-row && col <= colMid+row) ? '#' : ' ';
+    }
+    console.log(level);
+  }
+}
+
+// recursive solution
+const answer2 = (n) => {
+  loop(n);
+}
+
+const loop = (n, row = 0, level = '') => {
+  if (row === n) {
+    return;
+  }
+  const cols = (n * 2) - 1;
+  const colMid = Math.floor(cols / 2);
+
+  if (level.length === cols) {
+    console.log(level);
+    return loop(n, row + 1);
+  }
+
+  level += (level.length >= colMid-row && level.length <= colMid+row) ? '#' : ' ';
+  loop(n, row, level);
+}
+
+function pyramid(n) {
+  return answer2(n);
+}
+
+pyramid(3);
 
 module.exports = pyramid;
