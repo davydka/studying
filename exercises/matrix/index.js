7d5be8f57fe17b459a602dc2038b14dd12a165c6
davydka <davydka@gmail.com>
2020-05-08 12:12:02 -0700

    studying

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,65 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// results array of arrays to start
+// loop tempCol from startCol to endCol
+// increment startRow and tempRow
+//  keep incrementing tempRow and filling out endCol/tempCol
+//  at end, decrement endCol
+//  decrement tempCol
+//  once tempCol reaches 0, decrement endRow
+//  decrement tempRow until it reaches startRow
+// start over
+const answer1 = (n) => {
+  const results = [...Array(n)].map(_ => [...Array(n)]);
+  let counter = 1; // value to insert
+
+  let startCol = 0;
+  let endCol = n - 1;
+
+  let startRow = 0;
+  let endRow = n - 1;
+
+  while(startCol <= endCol && startRow <= endRow) {
+    // top row
+    for (let i = startCol; i <= endCol; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+
+    // right col
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endCol] = counter;
+      counter++;
+    }
+    endCol--;
+
+    // bottom row
+    for (let i = endCol; i >= startCol; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    // left col
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startCol] = counter;
+      counter++
+    }
+    startCol++;
+
+  }
+  // debugger;
+
+  return results;
+}
+
+function matrix(n) {
+  return answer1(n);
+}
+
+// const test = matrix(3);
+// debugger;
 
 module.exports = matrix;
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,45 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+const vowelsArray = [
+  'a',
+  'e',
+  'i',
+  'o',
+  'u',
+];
+
+const answer1 = (str) => {
+  let count = 0;
+  for (let c of str.toLowerCase()) {
+    if (vowelsArray.includes(c)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const answer2 = (str) => {
+  return str.toLowerCase().split('').reduce((a, c, i) => {
+    if (vowelsArray.includes(c)) {
+      a++;
+    }
+    return a;
+  }, 0);
+}
+
+// regex approach
+const answer3 = (str) => {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
+
+
+function vowels(str) {
+  return answer3(str);
+}
+
+const test = vowels('Hi There!');
+debugger;
 
 module.exports = vowels;
